f8a76dc7af536f35158078e4fd997164965f99b0
laura <47613228+laurarbrown@users.noreply.github.com>
2019-04-17 17:06:56 +0100

    JS to make video (on index page) play on hover
    Poster image shows when page loads, when hovered over, first frame of video shows. When clicked play on the controls, video plays but only if you keep the mouse over the element.  Move cursor away and it pauses, hover back on and it plays. Throws Javascript error "Uncaught (in promise) DOMException"

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var playing = false;
+    $('.moonvid').hover(function () {
+      if(!playing) {
+        playing = true;
+        $(this)[0].play();
+      }
+    }, function () {
+      var el = $(this)[0];
+      if(playing) {
+        playing = false;
+        el.pause();
+        el.currentTime = 0;
+      }
+    });
+});
